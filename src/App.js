cdfb1ba7235ca39a6e38358e2a5cb505cb936a58
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-12-24 23:43:53 -0500

    material-ui, sass, react-router, and nav-bar components added,login and sign-up components added as well

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import { NavBarContainer } from "./components/nav-bar/nav-bar-container.component";
+import { Route } from "react-router-dom";
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			teamData: []
+		};
+	}
+
+	fetchData = () => {
+		fetch("https://www.localhost:3000/teams")
+			.then(res => res.json())
+			.then(teamData => console.log(teamData))
+			.catch(err => console.error(err));
+	};
+
+	componentDidMount = () => {
+		fetchData();
+	};
+
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<NavBarContainer />
+			</div>
+		);
 	}
 }
